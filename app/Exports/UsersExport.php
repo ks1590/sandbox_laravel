cae99fd1b42e592d199f869ee7506a90e5f85f27
kensuke <35k.0101@gmail.com>
2021-11-06 22:04:43 +0900

    usersをヘッダー、シートタイトルを付けてエクスポート

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -5,27 +5,29 @@ namespace App\Exports;
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class UsersExport implements FromCollection,WithHeadings
+class UsersExport implements FromCollection,WithHeadings,WithTitle
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return User::all();
+        return User::select('id','name','email')->get();
     }
     
     public function headings(): array
 	{
 		return [
-				'#', 
-				'name', 
-				'email', 
-				'email_verified_at', 
-				'created_at', 
-				'updated_at'
-			]; 
+            '#', 
+            'name', 
+            'email', 
+        ]; 
+	}
+
+    public function title(): string{
+		return 'test';
 	}
 
     // public function collection()
